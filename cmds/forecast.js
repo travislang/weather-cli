35b558a180e9f408384ba2af0ccb141238d277a8
Travis Lang <tlang50@icloud.com>
2019-02-02 16:24:10 -0600

    fix formatting issues

--- cmds/forecast.js
+++ cmds/forecast.js
@@ -14,40 +14,46 @@ module.exports = async (args) => {
         const location = (args.location || args.l || await getLocation());
         const paddedLocation = location.padEnd(25, ' ');
         const weather = await getForecast(location);
-        // const temp = String(weather.data[0].temp + '°F').padEnd(8, ' ');
-        // const summary = weather.data[0].weather.description.padEnd(15, ' ');
-        // const wind = String(weather.data[0].wind_spd + ' MPH').padEnd(15, ' ');
+        if (!weather.data) {
+            spinner.stop()
+            console.log('unable to get the weather for that location.  Check the spelling and then try again'.cyan);
+            return;
+        }
         const today = {
             temp: String(weather.data[0].temp + '°F').padEnd(8, ' '),
             wind: weather.data[0].weather.description.padEnd(15, ' '),
-            summary: weather.data[0].weather.description.padEnd(15, ' ')
+            summary: weather.data[0].weather.description.padEnd(16, ' ')
         }
         const tomorrow = {
             temp: String(weather.data[8].temp + '°F').padEnd(8, ' '),
             wind: weather.data[8].weather.description.padEnd(15, ' '),
-            summary: weather.data[8].weather.description.padEnd(15, ' ')
+            summary: weather.data[8].weather.description.padEnd(16, ' ')
         }
         const dayAfter = {
             temp: String(weather.data[16].temp + '°F').padEnd(8, ' '),
             wind: weather.data[16].weather.description.padEnd(8, ' '),
-            summary: weather.data[16].weather.description.padEnd(15, ' ')
+            summary: weather.data[16].weather.description.padEnd(16, ' ')
         }
 
         spinner.stop()
+        if (!weather.data) {
+            console.log('unable to get the weather for that location.  Check the spelling and then try again'.cyan);
+            return;
+        }
         // DO NOT CHANGE FORMATTING - it will align correctly in the console
         console.log(`  
-         ---------------------------------------------------------------
-        |            3 Day Forecast for ${paddedLocation}       |
-        |                                                               |
-        |         Today             Tomorrow            Day After       |
-        |    ---------------     ---------------     ---------------    |
-        |   |               |   |               |   |               |   |
-        |   | temp: ${today.temp}|   | temp: ${tomorrow.temp}|   | temp: ${dayAfter.temp}|   |
-        |   |${today.summary}|   |${tomorrow.summary}|   |${dayAfter.summary}|   |
-        |   |               |   |               |   |               |   |
-        |    ---------------     ---------------     ---------------    |
-        |                                                               |
-         ---------------------------------------------------------------      
+         -------------------------------------------------------------------
+        |              3 Day Forecast for ${paddedLocation}         |
+        |                                                                   |
+        |           Today             Tomorrow            Day After         |
+        |    -----------------    -----------------    -----------------    |
+        |   |                 |  |                 |  |                 |   |
+        |   | temp: ${today.temp}  |  | temp: ${tomorrow.temp}  |  | temp: ${dayAfter.temp}  |   |
+        |   | ${today.summary}|  | ${tomorrow.summary}|  | ${dayAfter.summary}|   |
+        |   |                 |  |                 |  |                 |   |
+        |    -----------------    -----------------    -----------------    |
+        |                                                                   |
+         -------------------------------------------------------------------      
          `.cyan)
     } catch (err) {
         spinner.stop()
