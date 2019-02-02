342af4f6dc4a85df63d6061a3acea39de794121a
Travis Lang <tlang50@icloud.com>
2019-02-02 00:37:32 -0600

    working basic cli

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+const minimist = require('minimist');
+
 module.exports = () => {
-    console.log('Welcome to the outside!')
+    const args = minimist(process.argv.slice(2));
+    let cmd = args._[0] || 'help';
+
+    if (args.version || args.v) {
+        cmd = 'version'
+    }
+
+    if (args.help || args.h) {
+        cmd = 'help'
+    }
+
+    switch(cmd) {
+        case 'now':
+            require('./cmds/now')(args);
+            break
+        case 'version':
+            require('./cmds/version')(args)
+            break
+
+        case 'help':
+            require('./cmds/help')(args)
+            break
+        default:
+            console.error(`"${cmd}" is not a valid command.  Try running 'weather --help'`)
+            break
+    }
 }
